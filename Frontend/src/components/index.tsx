57e8ebc71d8dc441511eddfc84177d5e33cc4e93
bindepzai7 <doanductuan297@gmail.com>
2025-08-16 15:02:58 +0700

    add component

--- Frontend/src/components/index.tsx
+++ Frontend/src/components/index.tsx
@@ -2,3 +2,6 @@ export { default as TextInput } from "./TextInput/TextInput";
 export { default as DropDown } from "./DropDown/DropDown";
 export { default as ImageDropper } from "./ImageDropper/ImageDropper";
 export { default as Button } from "./Button/Button";
+export { default as VideoGallery } from "./VideoGallery/VideoGallery";
+export { default as MakeCSV } from "./MakeCSV/MakeCSV";
+export { default as VideoCard } from "./VideoCard/VideoCard";
