138fa7cf2f439d447c9630d3f8969cd31cd3c896
qhung23125005 <xinquyhung91005@gmail.com>
2025-08-16 13:37:22 +0700

    add components of the right panel

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,20 +1,27 @@
 import { useState } from 'react'
 import './App.css'
+import MakeCSV from './components/MakeCSV/MakeCSV'
+import VideoGallery from './components/VideoGallery/VideoGallery'
 
 function App() {
-
   return (
-    <>
-      <div className="container">
-        <div className="left">Left (2)
-
-        </div>
-        <div className="right">Right (3)
-
+    <div className="app-shell">
+      <div className="app-left">Left (2)</div>
+      <div className="app-right">
+        <div className="app-content">
+          <div className="app-header">
+            <h1 className="app-title">Golden Retrievers AI</h1>
+          </div>
+          <div className="app-makecsv">
+            <MakeCSV />
+          </div>
+          <div className="app-display">
+            {/* Display component will go here */}
+            <VideoGallery />
+          </div>
         </div>
       </div>
-    </>
+    </div>
   )
 }
-
 export default App
